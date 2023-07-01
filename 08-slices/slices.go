71e448fb00cb1e298305b4e327303950fd1ae13f
winaldomanurung <winaldo_satryadi@rocketmail.com>
2023-07-01 21:02:35 +0700

    lesson 08 - slices

---
+++ 08-slices/slices.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	// slices doesn't need to specify length
+	x:=[]int{10,20,40}
+
+	var y = []int{1, 5: 4, 6, 10: 100, 15}
+
+	fmt.Println(x)
+	fmt.Println(y)
+}
